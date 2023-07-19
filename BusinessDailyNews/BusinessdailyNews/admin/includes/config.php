3a4c4d952c647ffeb583d2636950be4be235030a
Bolaji Hammed <hammedbolajihammed@gmail.com>
2023-07-19 17:45:02 +0100

    Config troubleshoot

--- BusinessDailyNews/BusinessdailyNews/admin/includes/config.php
+++ BusinessDailyNews/BusinessdailyNews/admin/includes/config.php
@@ -7,6 +7,7 @@ $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
 {
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	echo "Failed to connect to MySQL: " . mysqli_connect_error()."<br>;
+	echo "Check your connection issue on 'admin/includes/config.php'";
 }
 ?>
